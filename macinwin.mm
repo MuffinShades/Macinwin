d3e72adffbde19c9a615c9c723f445bf8f9b7e10
muffinshades <89941934+MuffinShades@users.noreply.github.com>
2024-06-19 22:33:18 -0400

    time for test I

--- macinwin.mm
+++ macinwin.mm
@@ -6,16 +6,28 @@
 //everything else in C++
 
 Macinwin::Macinwin(const int width, const int height, const int x, const int y) {
+    //somesort of autorelease pool
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     [NSApplication sharedApplication];
 
     //window styling
     NSUInteger windowStyle = NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask;
 
-    //
+    //create le window
     NSRect windowRect = NSMakeRect(x, y, width, height);
-    NSWindow * window = [[NSWindow alloc] initWithContentRect:windowRect
+    NSWindow *win = [[NSWindow alloc] initWithContentRect:windowRect
                                           styleMask:windowStyle
                                           backing:NSBackingStoreBuffered
                                           defer:NO];
+    [win autorelease];
+
+    //create a window contorller thing
+    NSWindowController * windowController = [[NSWindowController alloc] initWithWindow:window];
+    [windowController autorelease];
+
+    //idk what this does
+    [window orderFrontRegardless];
+    [NSApp run];
+
+    [pool drain];
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "Macinwin.hpp"
 
 int main() {
+    Macinwin win = Macinwin(450, 450, 0, 0);
     return 0;
 }
